1b7237f1fe6cad59299a57b884be9a3124c5631a
Damanage <derongar@gmail.com>
2017-11-11 13:10:00 +0300

    clean code

--- client-side/dist/js/blogList.js
+++ client-side/dist/js/blogList.js
@@ -1,21 +1,5 @@
 'use strict';
 
-// class BlogList extends React.Component{
-
-// 	render(){
-// 		return React.createElement(
-// 			'div',
-// 			{className:'blogList'},
-// 				_.map(this.props.blogData,(item, key)=>(
-// 						)
-// 						React.createElement(BlogItem,{key, imgLink:item.link, blogText:item.text})
-// 					)
-// 		)
-// 	};  
-
-// };  Версия,где объект с данными получается через свойства. 
-
-
 var BlogList = function BlogList(_ref) {
 	var blogData = _ref.blogData;
 	return React.createElement('div', { className: 'blogList' }, _.map(blogData, function (item, key) {
--- client-side/js/blogList.js
+++ client-side/js/blogList.js
@@ -1,20 +1,4 @@
 
-// class BlogList extends React.Component{
-
-// 	render(){
-// 		return React.createElement(
-// 			'div',
-// 			{className:'blogList'},
-// 				_.map(this.props.blogData,(item, key)=>(
-// 						)
-// 						React.createElement(BlogItem,{key, imgLink:item.link, blogText:item.text})
-// 					)
-// 		)
-// 	};  
-
-// };  Версия,где объект с данными получается через свойства. 
-
-
 const BlogList = ({blogData})=>(
 		React.createElement(
 			'div',
